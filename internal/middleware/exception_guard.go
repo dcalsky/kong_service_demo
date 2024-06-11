1e67d1d9eb6f65f929de1d9886fddc55adf15dc9
dcalsky <dcalsky@163.com>
2024-06-11 11:16:09 +0800

    use runtime.Callers to skip top 3 frames

--- internal/middleware/exception_guard.go
+++ internal/middleware/exception_guard.go
@@ -1,9 +1,11 @@
 package middleware
 
 import (
+	"bytes"
 	"context"
 	"fmt"
 	"runtime"
+	"strconv"
 
 	"github.com/cloudwego/hertz/pkg/app"
 
@@ -15,7 +17,7 @@ func ExceptionGuard() app.HandlerFunc {
 	return func(ctx context.Context, c *app.RequestContext) {
 		defer func() {
 			if panicData := recover(); panicData != nil {
-				logs.Errorf(ctx, "[ExceptionGuard] stack: %s", getStack(1<<12))
+				logs.Errorf(ctx, "[ExceptionGuard] stack: \n%s", getStack())
 				logs.Errorf(ctx, "[ExceptionGuard] panic data: %v", panicData)
 				kongArgs := base.GetKongArgs(ctx, c)
 				if e, ok := panicData.(base.Exception); ok {
@@ -30,8 +32,28 @@ func ExceptionGuard() app.HandlerFunc {
 	}
 }
 
-func getStack(size int) []byte {
-	buf := make([]byte, size)
-	runtime.Stack(buf, false)
-	return buf
+func getStack() []byte {
+	buf := bytes.NewBuffer(make([]byte, 0, 2048))
+	pc := make([]uintptr, 12)
+	n := runtime.Callers(3, pc)
+	if n == 0 {
+		return make([]byte, 0)
+	}
+	pc = pc[:n]
+	frames := runtime.CallersFrames(pc)
+
+	for {
+		frame, more := frames.Next()
+		buf.WriteString(frame.Function)
+		buf.WriteByte('\n')
+		buf.WriteByte('\t')
+		buf.WriteString(frame.File)
+		buf.WriteByte(':')
+		buf.WriteString(strconv.Itoa(frame.Line))
+		buf.WriteByte('\n')
+		if !more {
+			break
+		}
+	}
+	return buf.Bytes()
 }
